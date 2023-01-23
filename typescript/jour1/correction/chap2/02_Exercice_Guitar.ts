a618ee6d72beb0fecef8f6f6f1fa2e8ac47d7e93
bbauer02 <bbauer02@gmail.com>
2023-01-23 14:49:04 +0100

    Ajout chapitre ENUM TYPE

---
+++ typescript/jour1/correction/chap2/02_Exercice_Guitar.ts
@@ -0,0 +1,36 @@
+abstract class Music {
+    protected _instrument: string = 'nothing';
+
+
+    abstract makeSound():string;
+
+    get instrument():string{
+        return this._instrument;
+    }
+
+    public play(): string {
+        return "play music";
+    }
+}
+
+// classe étendue
+class Guitar extends Music{
+    protected _instrument = 'guitar'; // définition du nom, surcharge la valeur de la classe mère Music
+
+    // implémentation du code utile de la méthode => contrat de la classe mère Music
+    makeSound(){
+
+        return "C D E F G A B";
+    }
+
+    public override  play(): string {
+        return "play music  override";
+    }
+}
+
+let guitar = new Guitar();
+
+console.log(guitar.instrument); // affichage du nom de l'instrument
+console.log(guitar.makeSound()); // jouer de la musique affiche C D E F G A B
+
+
--- typescript/jour1/whiteboard.ts
+++ typescript/jour1/whiteboard.ts
@@ -1,10 +1,7 @@
 abstract class Vehicule {
 
     abstract demarrer(arg1: number, arg2:number, arg3:string);
-
-
 }
-
 
 class Avion extends Vehicule {
     demarrer(arg1: number, arg2:number, arg3:string) {
@@ -12,10 +9,38 @@ class Avion extends Vehicule {
     }
 }
 
-
 class Voiture extends Vehicule {
     demarrer(arg1: number, arg2:number, arg3:string) {
         console.log("demarrage voiture");
     }
 }
+
+
+
+
+enum Status {
+    Published= "PUB",
+    Unpublished = "UNPUB",
+    Draft= "DRF"
+}
+
+// GET INDEX OF ENUM
+//
+const indexOfUnpublished = Object.keys(Status).indexOf('Unpublished');
+const valueOfUnpublished = Object.values(Status)[indexOfUnpublished]; // ES2017 +++
+
+let state : Status;
+
+state = Status.Draft;
+
+
+Object.values(Status).forEach((val) => console.log(val))
+
+
+Status.
+
+
+
+
+BDD => "Publi"
 
